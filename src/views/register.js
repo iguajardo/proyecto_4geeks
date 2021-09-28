1b5bc006b79d2c88f50a33dc305822257f41f02f
Diegogl91 <garciadiego03@gmail.com>
2021-09-28 14:44:07 +0000

    register

--- src/views/register.js
+++ src/views/register.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 
 const Register = () => {
+
+    const [input, setInput] = useState({
+        username: "",
+        password: "",
+        email: ""
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        fetch("https://serenity-rest-api.herokuapp.com/api/register", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(input)
+        })
+            .then((response) => response.json())
+            .then((data) =>  console.log(data) )
+            .catch((error) => console.log(error))
+    }
+
+    const handleChange = ({ target }) => {
+        setInput({
+            ...input,
+            [target.name]: target.value
+        })
+    }
+
     return (
         <>
             <div className="container d-flex py-5 justify-content-center">
@@ -11,12 +39,12 @@ const Register = () => {
                         <h3 className="text-center">Bienvenido a Serenity</h3>
                     </div>
                     <div className="card-body">
-                        <form className="flex-column g-3">
+                        <form onSubmit={handleSubmit} className="flex-column g-3" >
                             <div className="col-md-12">
                                 <label htmlFor="inputUsername" className="form-label fw-bold">
                                     Username
                                 </label>
-                                <input type="text" className="form-control" id="inputUsername" />
+                                <input type="text" className="form-control" id="inputUsername" name="username" value={input.username} onChange={handleChange} />
                             </div>
                             <div className="col-sm-12 col-md-12 mt-2">
                                 <label htmlFor="email" className="form-label fw-bold">
@@ -27,6 +55,9 @@ const Register = () => {
                                     className="form-control"
                                     id="email"
                                     placeholder="example@"
+                                    name="email"
+                                    value={input.email}
+                                    onChange={handleChange}
                                 />
                             </div>
                             <div className="col-sm-12 col-md-12 mt-2">
@@ -37,16 +68,17 @@ const Register = () => {
                                     type="password"
                                     className="form-control"
                                     id="password"
+                                    name="password"
+                                    value={input.password}
+                                    onChange={handleChange}
                                 />
                             </div>
+                            <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                                <button className="btn btn-primary fw-bold">
+                                    Send
+                                </button>
+                            </div>
                         </form>
-                    </div>
-                    <div className="card-footer">
-                        <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-                            <button className="btn btn-primary fw-bold" type="button">
-                                Send
-                            </button>
-                        </div>
                     </div>
                 </div>
             </div>
