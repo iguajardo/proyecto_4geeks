44759766afe7e57a553492061c4ce337e790f5b7
Italo Guajardo <italog.penaloza@gmail.com>
2021-10-05 17:46:27 -0300

    add user_name to navbar

--- src/views/perfil.js
+++ src/views/perfil.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Notas from '../components/Notas'
 import Calendario from '../components/Calendario'
 import { NavPerfil } from '../components/NavPerfil';
 import { Route } from 'react-router-dom';
+import { Context } from '../store/appContext';
 
 
 
 const Perfil = () => {
+    const { store } = useContext(Context)
     return (
         <div className="container">
             <div className=" d-flex justify-content-center flex-column align-items-center m-4">
                 <div className="imagen rounded-circle" style={{ background: "black", width: "100px", height: "100px" }}>
                 </div>
                 <div className="text-center">
-                    <h3>Nombre</h3>
-                    <p>Datos de Usuario</p>
+                    <h3>{store.nombre_usuario}</h3>
+                    <p>{store.user_email}</p>
                 </div>
             </div>
             <NavPerfil />
