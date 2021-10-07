814946cc7519c53ec9543b136a8a152834d6de12
Italo Guajardo <italog.penaloza@gmail.com>
2021-10-07 17:45:45 -0300

    fix display contact

--- src/components/Redessociales.js
+++ src/components/Redessociales.js
@@ -4,19 +4,19 @@ import logo from '../img/logo.png'
 
 const Redessociales = () => {
     return (
-        <div className="containerb">
-            <section id="redessociales">
-                <img id="logoGrande" src={logo} />
-                <h3 className="mt-3"> « Un respiro a la vez » </h3>
-                <div className="social-icons mt-3">
-                    <i className="fab fa-facebook-f"></i>
-                    <i className="fab fa-instagram"></i>
-                    <i className="fab fa-twitter"></i>
+        <section id="redessociales">
+            <div className="containerb">
+                <div className="contenedorRedes">
+                    <img id="logoGrande" src={logo} />
+                    <h3 className="mt-3"> « Un respiro a la vez » </h3>
+                    <div className="social-icons mt-3">
+                        <i className="fab fa-facebook-f"></i>
+                        <i className="fab fa-instagram"></i>
+                        <i className="fab fa-twitter"></i>
+                    </div>
                 </div>
-
-
-            </section>
-        </div>
+            </div>
+        </section>
     )
 }
 
